2e835e342370ff8182f773aef1c2a7a4d9b1a226
Your Name <duongtinhhust518@gmail.com>
2018-06-11 00:08:06 -0300

    add project

---
+++ test/system_stm32l4xx.d
@@ -0,0 +1,31 @@
+test\system_stm32l4xx.o: ..//Src/system_stm32l4xx.c
+test\system_stm32l4xx.o: C:/Users/Tinh HKVT19/STM32Cube/Repository/STM32Cube_FW_L4_V1.11.0/Drivers/CMSIS/Device/ST/STM32L4xx/Include/stm32l4xx.h
+test\system_stm32l4xx.o: C:/Users/Tinh HKVT19/STM32Cube/Repository/STM32Cube_FW_L4_V1.11.0/Drivers/CMSIS/Device/ST/STM32L4xx/Include/stm32l476xx.h
+test\system_stm32l4xx.o: C:/Users/Tinh HKVT19/STM32Cube/Repository/STM32Cube_FW_L4_V1.11.0/Drivers/CMSIS/Include/core_cm4.h
+test\system_stm32l4xx.o: D:\SETUP\DIENTU\KEIC V5\ARM\ARMCC\Bin\..\include\stdint.h
+test\system_stm32l4xx.o: C:/Users/Tinh HKVT19/STM32Cube/Repository/STM32Cube_FW_L4_V1.11.0/Drivers/CMSIS/Include/core_cmInstr.h
+test\system_stm32l4xx.o: C:/Users/Tinh HKVT19/STM32Cube/Repository/STM32Cube_FW_L4_V1.11.0/Drivers/CMSIS/Include/cmsis_armcc.h
+test\system_stm32l4xx.o: C:/Users/Tinh HKVT19/STM32Cube/Repository/STM32Cube_FW_L4_V1.11.0/Drivers/CMSIS/Include/core_cmFunc.h
+test\system_stm32l4xx.o: C:/Users/Tinh HKVT19/STM32Cube/Repository/STM32Cube_FW_L4_V1.11.0/Drivers/CMSIS/Include/core_cmSimd.h
+test\system_stm32l4xx.o: C:/Users/Tinh HKVT19/STM32Cube/Repository/STM32Cube_FW_L4_V1.11.0/Drivers/CMSIS/Device/ST/STM32L4xx/Include/system_stm32l4xx.h
+test\system_stm32l4xx.o: C:/Users/Tinh HKVT19/STM32Cube/Repository/STM32Cube_FW_L4_V1.11.0/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal.h
+test\system_stm32l4xx.o: ../Inc/stm32l4xx_hal_conf.h
+test\system_stm32l4xx.o: ../Inc/main.h
+test\system_stm32l4xx.o: C:/Users/Tinh HKVT19/STM32Cube/Repository/STM32Cube_FW_L4_V1.11.0/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_rcc.h
+test\system_stm32l4xx.o: C:/Users/Tinh HKVT19/STM32Cube/Repository/STM32Cube_FW_L4_V1.11.0/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_def.h
+test\system_stm32l4xx.o: C:/Users/Tinh HKVT19/STM32Cube/Repository/STM32Cube_FW_L4_V1.11.0/Drivers/CMSIS/Device/ST/STM32L4xx/Include/stm32l4xx.h
+test\system_stm32l4xx.o: C:/Users/Tinh HKVT19/STM32Cube/Repository/STM32Cube_FW_L4_V1.11.0/Drivers/STM32L4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h
+test\system_stm32l4xx.o: D:\SETUP\DIENTU\KEIC V5\ARM\ARMCC\Bin\..\include\stdio.h
+test\system_stm32l4xx.o: C:/Users/Tinh HKVT19/STM32Cube/Repository/STM32Cube_FW_L4_V1.11.0/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_rcc_ex.h
+test\system_stm32l4xx.o: C:/Users/Tinh HKVT19/STM32Cube/Repository/STM32Cube_FW_L4_V1.11.0/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_gpio.h
+test\system_stm32l4xx.o: C:/Users/Tinh HKVT19/STM32Cube/Repository/STM32Cube_FW_L4_V1.11.0/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_gpio_ex.h
+test\system_stm32l4xx.o: C:/Users/Tinh HKVT19/STM32Cube/Repository/STM32Cube_FW_L4_V1.11.0/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_dma.h
+test\system_stm32l4xx.o: C:/Users/Tinh HKVT19/STM32Cube/Repository/STM32Cube_FW_L4_V1.11.0/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_dma_ex.h
+test\system_stm32l4xx.o: C:/Users/Tinh HKVT19/STM32Cube/Repository/STM32Cube_FW_L4_V1.11.0/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_cortex.h
+test\system_stm32l4xx.o: C:/Users/Tinh HKVT19/STM32Cube/Repository/STM32Cube_FW_L4_V1.11.0/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_flash.h
+test\system_stm32l4xx.o: C:/Users/Tinh HKVT19/STM32Cube/Repository/STM32Cube_FW_L4_V1.11.0/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_flash_ex.h
+test\system_stm32l4xx.o: C:/Users/Tinh HKVT19/STM32Cube/Repository/STM32Cube_FW_L4_V1.11.0/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_flash_ramfunc.h
+test\system_stm32l4xx.o: C:/Users/Tinh HKVT19/STM32Cube/Repository/STM32Cube_FW_L4_V1.11.0/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_i2c.h
+test\system_stm32l4xx.o: C:/Users/Tinh HKVT19/STM32Cube/Repository/STM32Cube_FW_L4_V1.11.0/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_i2c_ex.h
+test\system_stm32l4xx.o: C:/Users/Tinh HKVT19/STM32Cube/Repository/STM32Cube_FW_L4_V1.11.0/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_pwr.h
+test\system_stm32l4xx.o: C:/Users/Tinh HKVT19/STM32Cube/Repository/STM32Cube_FW_L4_V1.11.0/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_pwr_ex.h
